67ba6201c28c99676d405453d2ddf6e3897a3338
Gustavo Ribeiro <99373133+gustavosett@users.noreply.github.com>
2023-05-02 01:17:52 -0300

    Update photo_separator_gui.py

--- photo_separator_gui.py
+++ photo_separator_gui.py
@@ -1,85 +1,56 @@
-import shutil
 import tkinter as tk
 from tkinter import filedialog, messagebox
 import os
-import cv2
 import numpy as np
 import dlib
+import concurrent.futures
+from concurrent.futures import ThreadPoolExecutor
+import threading
 
 
 class PhotoSeparatorGUI:
-    """
-    Interface gráfica de seleção de diretorios de fotos
-    e aplicar separação com base em reconhecimento facial.
-    @gustavosett #rev 30/04/2023
-    """
-
-
     def __init__(self):
-        # Inicializa a janela principal
         self.root = tk.Tk()
         self.root.title('Separador de Fotos por Reconhecimento Facial')
 
-        # Inicializa as variáveis de diretório
         self.input_dir_var = tk.StringVar()
         self.output_dir_var = tk.StringVar()
         self.faces_dir_var = tk.StringVar()
 
-        # Cria e organiza os widgets na janela
+        self.lock = threading.Lock()
+
         self.create_widgets()
         self.arrange_widgets()
 
+        self.facerec = dlib.face_recognition_model_v1(os.path.join('dlib_face_recognition_resnet_model_v1.dat'))
 
     def select_input_dir(self):
-        """Solicita ao usuário que selecione o diretório de entrada das fotos."""
         input_dir = filedialog.askdirectory()
         if input_dir:
             self.input_dir_var.set(input_dir)
             print('Diretório de entrada selecionado:', input_dir)
 
-
     def select_output_dir(self):
-        """Solicita ao usuário que selecione o diretório de saída das fotos."""
         output_dir = filedialog.askdirectory()
         if output_dir:
             self.output_dir_var.set(output_dir)
             print('Diretório de saída selecionado:', output_dir)
 
-
-    def select_faces_dir(self):
-        """Solicita ao usuário que selecione o diretório de treinamento das faces."""
-        faces_dir = filedialog.askdirectory()
-        if faces_dir:
-            self.faces_dir_var.set(faces_dir)
-            print('Diretório de treinamento das faces selecionado:', faces_dir)
-
-
     def run(self):
-        """Executa a separação das fotos."""
-        print('Executou!')
         input_dir = self.input_dir_var.get()
         output_dir = self.output_dir_var.get()
-        faces_dir = self.faces_dir_var.get()
+        faces_dir = os.path.dirname(os.path.realpath(__file__))
 
-        # Verifica se os diretórios foram selecionados corretamente
         if not self.check_directories(input_dir, output_dir, faces_dir):
             return
         
-        # Separe as fotos com base no reconhecimento facial
         self.separate_photos(input_dir, output_dir, faces_dir)
-        print('Executou corretamente!')
-
-
-
 
     def print_error(self, title, message):
-        """Imprime uma mensagem de erro."""
         messagebox.showerror(title, message)
         print(f'{title}: {message}')
 
-
     def check_directories(self, input_dir, output_dir, faces_dir):
-        """Verifica se os diretórios foram selecionados, se são distintos e se são válidos."""
         error_messages = [
             ('Por favor, selecione um diretório de entrada.', input_dir),
             ('Por favor, selecione um diretório de saída.', output_dir),
@@ -101,59 +72,71 @@ class PhotoSeparatorGUI:
                 return False
 
         return True
-    
+
+    def process_image(self, input_file_path, file, face_dict, detector, sp, output_dir, folder_counter):
+        img = dlib.load_rgb_image(input_file_path)
+        dets = detector(img, 1)
+
+        if len(dets) > 0:
+            full_detections = dlib.full_object_detections()
+
+            for k, d in enumerate(dets):
+                shape = sp(img, d)
+                full_detection = dlib.full_object_detection(d, shape.parts())
+                full_detections.append(full_detection)
+
+            for detection in full_detections:
+                face_chip_150 = dlib.get_face_chip(img, detection, size=150, padding=0.25)
+                found_similar_face = False
+                for folder_name, reference_face in face_dict.items():
+                    if self.compare_faces(reference_face, face_chip_150):
+                        found_similar_face = True
+                        break
+
+                if not found_similar_face:
+                    with self.lock:
+                        folder_counter += 1
+                        folder_name = f'Person_{folder_counter}'
+                        face_dict[folder_name] = face_chip_150
+
+                person_folder_path = os.path.join(output_dir, folder_name)
+                if not os.path.exists(person_folder_path):
+                    os.makedirs(person_folder_path)
+
+                output_file_path = os.path.join(person_folder_path, f"{os.path.splitext(file)[0]}_face_{detection.rect.left()}_{detection.rect.top()}{os.path.splitext(file)[1]}")
+                dlib.save_image(face_chip_150, output_file_path)
+                print('\033[1;49;32m' + f'Face found in {file}!!' + '\033[m')
+        else:
+            print('\033[1;49;31m' + f"No face detected in {file}" + '\033[m')
+
+        return folder_counter, {folder_name: face_chip_150}
+
 
     def separate_photos(self, input_dir, output_dir, faces_dir):
         detector = dlib.get_frontal_face_detector()
         sp = dlib.shape_predictor(os.path.join(faces_dir, 'shape_predictor_68_face_landmarks.dat'))
-        
+
+        face_dict = {}
+        folder_counter = 0
+
         for root, dirs, files in os.walk(input_dir):
-            for file in files:
-                if file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith(".png"):
-                    input_file_path = os.path.join(root, file)
-                    img = dlib.load_rgb_image(input_file_path)
-                    dets = detector(img, 1)
-
-                    if len(dets) > 0:
-                        # Create a dlib.full_object_detections object
-                        full_detections = dlib.full_object_detections()
-
-                        for k, d in enumerate(dets):
-                            shape = sp(img, d)
-                            # Create a full_object_detection using the rectangle and the parts of the shape
-                            full_detection = dlib.full_object_detection(d, shape.parts())
-                            # Add the full_object_detection to the full_object_detections object
-                            full_detections.append(full_detection)
-
-                        # Pass the full_detections object to the dlib.get_face_chip_details() function
-                        chip_details_list = dlib.get_face_chip_details(full_detections, size=256)
-
-                        for chip_details in chip_details_list:
-                            face_chip = dlib.extract_image_chip(img, chip_details)
-                            output_file_path = os.path.join(output_dir, f"{os.path.splitext(file)[0]}_face_{chip_details.rect.left()}_{chip_details.rect.top()}{os.path.splitext(file)[1]}")
-                            dlib.save_image(face_chip, output_file_path)
-                            print('\033[1;49;32m' + f'Face found in {file}!!' + '\033[m')
-                    else:
-                        print('\033[1;49;31m' + f"No face detected in {file}" + '\033[m')
-
-
+            with ThreadPoolExecutor() as executor:
+                futures = [executor.submit(self.process_image, os.path.join(root, file), file, face_dict, detector, sp, output_dir, folder_counter) for file in files if file.endswith((".jpg", ".jpeg", ".png"))]
+                for future in concurrent.futures.as_completed(futures):
+                    updated_folder_counter, updated_face_dict = future.result()
+                    with self.lock:  # Adquire o Lock
+                        folder_counter = max(folder_counter, updated_folder_counter)
+                        face_dict.update(updated_face_dict)
 
 
     def compare_faces(self, face1, face2, threshold=0.6):
-        """Compara duas faces usando o modelo de reconhecimento facial."""
-        # Calcule as descrições das faces
         face1_descriptor = self.facerec.compute_face_descriptor(face1)
         face2_descriptor = self.facerec.compute_face_descriptor(face2)
 
-        # Calcule a distância euclidiana entre as descrições das faces
         distance = np.linalg.norm(np.array(face1_descriptor) - np.array(face2_descriptor))
-
-        # Retorne True se a distância for menor que o limite, caso contrário, retorne False
         return distance < threshold
 
-
     def create_widgets(self):
-        """Cria os widgets para a janela principal."""
         self.input_dir_label = tk.Label(self.root, text='Diretório de entrada das fotos:')
         self.input_dir_entry = tk.Entry(self.root, textvariable=self.input_dir_var)
         self.input_dir_button = tk.Button(self.root, text='Selecionar', command=self.select_input_dir)
@@ -162,15 +145,9 @@ class PhotoSeparatorGUI:
         self.output_dir_entry = tk.Entry(self.root, textvariable=self.output_dir_var)
         self.output_dir_button = tk.Button(self.root, text='Selecionar', command=self.select_output_dir)
 
-        self.faces_dir_label = tk.Label(self.root, text='Diretório de treinamento das faces:')
-        self.faces_dir_entry = tk.Entry(self.root, textvariable=self.faces_dir_var)
-        self.faces_dir_button = tk.Button(self.root, text='Selecionar', command=self.select_faces_dir)
-
         self.run_button = tk.Button(self.root, text='Executar', command=self.run)
 
-
     def arrange_widgets(self):
-        """Organiza os widgets na janela principal."""
         self.input_dir_label.grid(row=0, column=0, padx=10, pady=10, sticky=tk.W)
         self.input_dir_entry.grid(row=0, column=1, padx=10, pady=10, sticky=tk.W + tk.E)
         self.input_dir_button.grid(row=0, column=2, padx=10, pady=10)
@@ -179,12 +156,7 @@ class PhotoSeparatorGUI:
         self.output_dir_entry.grid(row=1, column=1, padx=10, pady=10, sticky=tk.W + tk.E)
         self.output_dir_button.grid(row=1, column=2, padx=10, pady=10)
 
-        self.faces_dir_label.grid(row=2, column=0, padx=10, pady=10, sticky=tk.W)
-        self.faces_dir_entry.grid(row=2, column=1, padx=10, pady=10, sticky=tk.W + tk.E)
-        self.faces_dir_button.grid(row=2, column=2, padx=10, pady=10)
-
         self.run_button.grid(row=3, column=1, padx=10, pady=10)
-
 
     def mainloop(self):
         """Inicia o loop principal da janela."""
